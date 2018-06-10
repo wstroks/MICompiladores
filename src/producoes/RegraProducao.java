0626b26badb5ca995956b44ed6a3f56d6a6ad7d9
Tayane Cerqueira <engcomp.tayane@gmail.com>
2018-06-10 00:15:07 -0300

    Adicionando classe abstrata para regra de produção

---
+++ src/producoes/RegraProducao.java
@@ -0,0 +1,60 @@
+package producoes;
+
+import java.util.ArrayList;
+
+import lexico.TipoToken;
+
+/**
+*
+* @author Tayane
+*
+*/
+public abstract class RegraProducao {
+	
+    protected ArrayList<TipoToken> first  = new ArrayList<TipoToken>();
+    protected ArrayList<TipoToken> follow = new ArrayList<TipoToken>();
+    
+    protected RegraProducao() {
+        this.gerarFirst();
+        this.gerarFollow();
+    }
+    
+    /**
+     * Realiza a analise sintática para a regra
+     * 
+     * @return true se a sequência foi reconhecida, falso caso contrário
+     */
+    public abstract boolean analisar();
+
+
+    /**
+     * Gera o conjunto primeiro da regra.
+     *
+     */
+    protected abstract void gerarFirst();
+
+    /**
+     * Gera o conjunto seguinte da regra.
+     *
+     */
+    protected abstract void gerarFollow();
+
+    /**
+     * Informa se o token esta no conjunto primeiro da regra
+     * @param tipoToken um TokenClass
+     * @return true se sim, falso caso contrario
+     */
+    public boolean isFirst(TipoToken tipoToken) {
+        return first.contains(tipoToken);
+    }
+
+    /**
+     * Informa se o token esta no conjunto seguinte da regra
+     * @param tipoToken um TokenClass
+     * @return true se sim, falso caso contrário
+     */
+    public boolean isFollow(TipoToken tipoToken) {
+        return follow.contains(tipoToken);
+    }
+
+}
