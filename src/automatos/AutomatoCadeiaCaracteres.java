3c6c53c74a9255ac88bb444df8bae183ea95a58d
Tayane Cerqueira <engcomp.tayane@gmail.com>
2018-06-11 21:56:39 -0300

    Identando código

--- src/automatos/AutomatoCadeiaCaracteres.java
+++ src/automatos/AutomatoCadeiaCaracteres.java
@@ -21,7 +21,7 @@ public class AutomatoCadeiaCaracteres extends Automato {
 
 		lexema = "";
 		int estado = 0;
-                int verificar=0;
+		int verificar = 0;
 
 		while (!buffer.fimCodigo()) {
 			char c = buffer.lookAhead();
@@ -36,43 +36,39 @@ public class AutomatoCadeiaCaracteres extends Automato {
 				break;
 
 			case 1:
-                               //System.out.println("Estado 1: "+c);
-			        if(this.isBarraInvertida(c)){
-                                   // System.out.println("Estado 2: "+c);
-                                    verificar=1;
-                                    consumirCaractere();
-                                    
-                                                               
-                                }
-                                else if (c == '"') {
+				// System.out.println("Estado 1: "+c);
+				if (this.isBarraInvertida(c)) {
+					// System.out.println("Estado 2: "+c);
+					verificar = 1;
 					consumirCaractere();
-                                        if(buffer.fimCodigo()==true && verificar==1){
-                                            return getToken(TipoToken.CADEIA_CARACTERES_MAL_FORMADA);
-                                        }
-                                       else if(verificar==0) {                                   
-					return getToken(TipoToken.CADEIA_CARACTERES);
-                                        }else{
-                                            verificar=0;
-                                            estado=1;
-                                        }
-				} 
-                                else if (this.isLetra(c) || this.isDigito(c) || this.isSimbolo(c)) {
+
+				} else if (c == '"') {
 					consumirCaractere();
-                                        if(buffer.fimCodigo()==true){
-                                            return getToken(TipoToken.CADEIA_CARACTERES_MAL_FORMADA);
-                                        }
-                                        verificar=0;
+					if (buffer.fimCodigo() == true && verificar == 1) {
+						return getToken(TipoToken.CADEIA_CARACTERES_MAL_FORMADA);
+					} else if (verificar == 0) {
+						return getToken(TipoToken.CADEIA_CARACTERES);
+					} else {
+						verificar = 0;
+						estado = 1;
+					}
+				} else if (this.isLetra(c) || this.isDigito(c) || this.isSimbolo(c)) {
+					consumirCaractere();
+					if (buffer.fimCodigo() == true) {
+						return getToken(TipoToken.CADEIA_CARACTERES_MAL_FORMADA);
+					}
+					verificar = 0;
 				} else if (this.isBarraInvertida(c)) {
 					estado = 2;
-                                        verificar=0;
+					verificar = 0;
 					consumirCaractere();
 				} else {
-					if(c != '\n'){
+					if (c != '\n') {
 						consumirCaractere();
-                                                if(buffer.fimCodigo()==true && verificar==1){
-                                                    return getToken(TipoToken.CADEIA_CARACTERES_MAL_FORMADA);
-                                        }
-                                                verificar=0;
+						if (buffer.fimCodigo() == true && verificar == 1) {
+							return getToken(TipoToken.CADEIA_CARACTERES_MAL_FORMADA);
+						}
+						verificar = 0;
 					}
 					return getToken(TipoToken.CADEIA_CARACTERES_MAL_FORMADA);
 				}
