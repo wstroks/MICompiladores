6be29575a301876dc5d62d7bdd1e0b7b1f77ccfc
Tayane Cerqueira <engcomp.tayane@gmail.com>
2018-06-19 22:05:53 -0300

    Corrigindo produções do pacote estruturasRepeticao

--- src/producoes/estruturasRepeticao/While.java
+++ src/producoes/estruturasRepeticao/While.java
@@ -5,7 +5,6 @@ package producoes.estruturasRepeticao;
 
 import lexico.TipoToken;
 import producoes.RegraProducao;
-import producoes.blocoStruct.DeclaracaoDeStructAux;
 import producoes.declaracaoFuncaoProcedimento.Bloco;
 import producoes.precedenciaOperadores.Expressao;
 import sintatico.GerenciadorToken;
@@ -22,42 +21,47 @@ public class While extends RegraProducao {
 
     @Override
     public boolean analisar(GerenciadorToken gerenciadorToken) {
-        // TODO Auto-generated method stub
+
         if (isFirst(gerenciadorToken.getTokenAtual().getTipoToken())) {
 
             if (!consumir(gerenciadorToken, TipoToken.PALAVRA_RESERVADA_WHILE)) {
                 return false;
-            }else if (!consumir(gerenciadorToken, TipoToken.DELIMITADOR_ABRE_PARENTESES)) {
+            }
+            
+            if (!consumir(gerenciadorToken, TipoToken.DELIMITADOR_ABRE_PARENTESES)) {
                 return false;
             } 
             
-            else if (!Expressao.getInstancia().analisar(gerenciadorToken)) {
+            if (!Expressao.getInstancia().analisar(gerenciadorToken)) {
                 return false;
-            }else if (!consumir(gerenciadorToken, TipoToken.DELIMITADOR_FECHA_PARENTESES)) {
+            }
+            
+            if (!consumir(gerenciadorToken, TipoToken.DELIMITADOR_FECHA_PARENTESES)) {
                 return false;
-            } else if(!Bloco.getInstancia().analisar(gerenciadorToken)) {
+            }
+            
+            if(!Bloco.getInstancia().analisar(gerenciadorToken)) {
                 return false;
             }
 
             return true;
 
         }
+        
         return false;
+        
     }
 
     @Override
     protected void gerarFirst() {
-        // TODO Auto-generated method stub
         // while
         first.add(TipoToken.PALAVRA_RESERVADA_WHILE);
     }
 
     @Override
     protected void gerarFollow() {
-        // TODO Auto-generated method stub
         //--, !, ( , ++, CadeiaDeCaracter, Digitos, false, identificador, print, return , scan, struct,true, typdef,
         //var,while, } 
-        
         follow.add(TipoToken.OPERADOR_ARITIMETICO_DECREMENTO);
         follow.add(TipoToken.OPERADOR_ARITIMETICO_INCREMENTO);
         follow.add(TipoToken.OPERADOR_LOGICO_EXCLAMACAO_NEGADO);
@@ -75,9 +79,6 @@ public class While extends RegraProducao {
         follow.add(TipoToken.PALAVRA_RESERVADA_TYPEDEF);
         follow.add(TipoToken.PALAVRA_RESERVADA_VAR);
         follow.add(TipoToken.PALAVRA_RESERVADA_WHILE);
-        
-        
-
     }
 
 }
